45e065fc57abb851a8f867533cd70d0997b34cae
Evan William Gretok <mynameisevan3@gmail.com>
2018-08-31 14:46:44 -0400

    Messages added.

--- main.c
+++ main.c
@@ -61,10 +61,25 @@ int main( int argc, char *argv[] ) {
   int        numParticles  = 0;     // Number of Particles
   int        numIterations = 0;     // Number of Iterations
   particle_t gbest;                 // Global Best Particle Tracker
+  // Timing Measures
+  double     jobStart      = 0;     // Job Clock Start Time
+  double     initStart     = 0;     // Initialization Start Time
+  double     initEnd       = 0;     // Initialization End Time
+  double     swarmStart    = 0;     // Swarm Start Time
+  double     swarmEnd      = 0;     // Swarm End Time
+  double     cleanupStart  = 0;     // Cleanup Start Time
+  double     cleanupEnd    = 0;     // Cleanup End Time
+  double     jobEnd        = 0;     // Job Clock End Time
+  // Timing Results
+  double     initTime      = 0;     // Program Time for Initialization
+  double     swarmTime     = 0;     // Program Time for Particle Swarm Operation
+  double     cleanupTime   = 0;     // Program Time for Cleanup
+  double     overheadTime  = 0;     // Overhead Time Not Accounted For
+  double     jobTime       = 0;     // Total Program Time
 
   // Parse Input Parameters
   if( argc != 7 ) {
-    fprintf( stderr, "\nWrong number of arguments.\n" );
+    fprintf( stderr, "Wrong number of arguments.\n" );
     printf( "Description: Scales an image down into a thumbnail.\n\n" );
     printf( "Usage: %s <global> <local> <delta> <particles> <iterations> <display>\n", argv[0] );
     printf( "  Global     - input .ppm file.\n" );
@@ -85,6 +100,18 @@ int main( int argc, char *argv[] ) {
     display       = atoi( argv[6] );
   }
 
+  // Intro Text
+  if( display ) {
+    printf( "\n   = = =  Particle Swarm Optimization  = = =   \n\n" );
+  }
+
+  // Set Job Start and Initialization Timer
+  jobStart  = omp_get_wtime( );
+  if( display ) {
+    printf( "Initializing Swarm Components...\n" );
+  }
+  initStart = omp_get_wtime( );
+
   // Create Arrays Dynamically Based on Input Parameters
   particle_t particles[numParticles];
   particle_t pbest[numParticles];
@@ -94,15 +121,62 @@ int main( int argc, char *argv[] ) {
 
   // Initialize Swarm
   initPopulation( &numParticles, particles, pbest, &gbest );
+  initEnd = omp_get_wtime( );
 
   // Perform Particle Swarm Operation
+  if( display ) {
+    printf( "Performing %d Particle Swarm for %d Iterations...\n",
+              numParticles, numIterations                          );
+  }
+  swarmStart = omp_get_wtime( );
   for( i = 0; i < numIterations; i++ ) {
     moveSwarm( &c1, &c2, &dt, &numParticles, particles, pbest, &gbest );
-    printf( "%lg\n", gbest.fitness );
+  }
+  swarmEnd   = omp_get_wtime( );
+
+  // Display Optimum Data
+  if( display ) {
+    printf( "Best of %lf Found at ( %lf, %lf )\n",
+              gbest.fitness, gbest.position.x,
+              gbest.position.y                     );
   }
 
   // Cleanup Swarm
+  if( display ) {
+    printf( "Cleaning Up Swarm...\n" );
+  }
+  cleanupStart = omp_get_wtime( );
   closePopulation( &numParticles, particles );
+  cleanupEnd   = omp_get_wtime( );
+
+  // Final Tasks
+  if( display ) {
+    printf( "Operation Complete!\n" );
+  }
+
+  // Final Timing Component
+  jobEnd       = omp_get_wtime( );                              // End Full Program Clock
+  initTime     = initEnd    - initStart;                        // Calculate Initialization Time
+  swarmTime    = swarmEnd   - swarmStart;                       // Calculate Swarm Time
+  cleanupTime  = cleanupEnd - cleanupStart;                     // Calculate Cleanup Time
+  jobTime      = jobEnd     - jobStart;                         // Calculate Overall Time
+  overheadTime = jobTime - initTime - swarmTime - cleanupTime;  // Calculate Overhead Time
+
+  // Handle Displaying All Timing Data
+  if( display ) {  // Messages only when display is 1.
+    printf( "=== Timing Data ===\n  Initialize:\t\t" );
+    printf( "%0.9lf\n", initTime );
+    printf( "  Swarm:\t\t" );
+  }
+  printf( "%0.9lf ", swarmTime );
+  if( display ) {
+    printf( "\n  Cleanup:\t\t" );
+    printf( "%0.9lf\n", cleanupTime );
+    printf( "  Overhead:\t\t" );
+    printf( "%0.9lf\n", overheadTime );
+    printf( "  Total Job Time:\t" );
+    printf( "%0.9lf\n\n", jobTime );
+  }
 
   return 0;
 }
